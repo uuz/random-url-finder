7ca5cb620d416c7f47817a6ede8efc09a4232a3d
falling <57621064+uuz@users.noreply.github.com>
2020-10-29 16:49:27 +0000

    Update finder.py
    Added 2 different modes

--- finder.py
+++ finder.py
@@ -18,6 +18,7 @@ SUCCESS = "\033[1;37;40m[\033[1;32;40m+\033[1;37;40m]"
 GREEN = "\033[1;92;40m"
 WHITE = "\033[1;37;40m"
 BLUE = "\033[1;90;40m"
+BLUE2 = "\033[1;36;40m"
 YELLOW = "\033[1;93;40m"
 RED = "\033[1;31;40m"
 
@@ -44,23 +45,57 @@ def find():
             error += 1
             find()
 
+def ip():
+    global done, skipped, error
+    while True:
+        try:
+            a = str(randint(1,255))
+            b = str(randint(1,255))
+            c = str(randint(1,255))
+            d = str(randint(1,255))
+            pool = "{}.{}.{}.{}".format(a,b,c,d)
+            response = httpx.get(url='http://' + pool)
+            if response.text == '' or 'available' in response.text or 'for sale' in response.text or 'domain.dot.tk' in response.text or response.status_code == 0 or response.status_code == 404 or response.status_code == 503 or response.status_code == 525:
+                skipped += 1
+            else:
+                done += 1
+                success = open("found.txt", "a")
+                success.write(f"http://{pool}\n")
+                success.close()
+        except Exception as w:
+            error += 1
+            ip()
+
 clear()
 print(f'{BANNER} Falling Random Url Finder\n')
-print(f'{INFO} Define range\n')
-rng1 = input(f"{ASK} Minimum lenght: ")
-rng2 = input(f"\n{ASK} Maximum lenght: ")
-print('')
-while rng1 == rng2 or int(rng1) < 0 or int(rng2) < 0 or int(rng1) > int(rng2):
-    print(f"{INFO} Please, enter a valid range!\n")
+print(f'{INFO} Select mode\n')
+print(f'[{BLUE2}1{WHITE}] Random between range\n[{BLUE2}2{WHITE}] Random IP\n')
+mode = input(f"{ASK} Mode: ")
+while int(mode) <= 0 or int(mode) > 2:
+    mode = input(f"{ASK} Please enter a valid mode: ")
+if mode == '1':
+    print(f'\n{INFO} Define range\n')
     rng1 = input(f"{ASK} Minimum lenght: ")
     rng2 = input(f"\n{ASK} Maximum lenght: ")
     print('')
-thrds = input(f"{ASK} Threads: ")
-for i in range(int(thrds)):
-    thread = Thread(target=find)
-    thread.setDaemon(True)
-    thread.start()
-print("\n{} All threads successfully initialized.\n".format(SUCCESS))
+    while rng1 == rng2 or int(rng1) < 0 or int(rng2) < 0 or int(rng1) > int(rng2):
+        print(f"{INFO} Please, enter a valid range!\n")
+        rng1 = input(f"{ASK} Minimum lenght: ")
+        rng2 = input(f"\n{ASK} Maximum lenght: ")
+    thrds = input(f"{ASK} Threads: ")
+    for i in range(int(thrds)):
+        thread = Thread(target=find)
+        thread.setDaemon(True)
+        thread.start()
+    print("\n{} All threads successfully initialized.\n".format(SUCCESS))
+else:
+    thrds = input(f"\n{ASK} Threads: ")
+    for i in range(int(thrds)):
+        thread = Thread(target=ip)
+        thread.setDaemon(True)
+        thread.start()
+    print("\n{} All threads successfully initialized.\n".format(SUCCESS))
+
 
 while True:
     for spinner in ['.  ', '.. ', '...', ' ..', '  .', '   ']:
